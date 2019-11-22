ca50bb639195dab0ae5ae47eff85c8dc73e91df2
ViewTool-Jerry <sjliu@viewtool.com>
2019-11-22 22:57:07 +0800

    1.save project configuration

--- trunk/FicusF4 Driver Example/Application_I2C/RVMDK/Objects/startup_stm32f4xx.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f4xx.o: ..\CMSIS\Device\ST\STM32F4xx\Source\Templates\arm\startup_stm32f4xx.s
--- trunk/FicusF4 Driver Example/Application_I2C/RVMDK/Objects/stm32fxxx_it.d
+++
@@ -1,3 +0,0 @@
-.\objects\stm32fxxx_it.o: ..\USER\stm32fxxx_it.c
-.\objects\stm32fxxx_it.o: ..\USER\stm32fxxx_it.h
-.\objects\stm32fxxx_it.o: E:\1.Program_Software\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
